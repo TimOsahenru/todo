28a5427f01e6d1b44aa0118a5d98748dd3f6515b
TimOsahenru <timosahenru@gmail.com>
2025-02-27 15:30:46 +0000

    Create todo endpoint implementation

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from app import views
 
 
 urlpatterns = [
-    path('create/', views.create, name='create_todo'),
+    path('create/', views.create_todo, name='create_todo'),
     path('', views.all_todos, name='todo_list'),
 ]
